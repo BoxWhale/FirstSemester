e0dc8e4ee8df610696f49fd7d224ef15a7d5e3bc
Tue <vorreappelt@gmail.com>
2024-09-18 13:56:32 +0200

    Added a new variant of the CR_Pr1 that now uses arrays to remember last and new values added: can now also take the sum of all values

---
+++ MiniProjects/CR_Pr2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Runtime;
+using System.Linq;
+using System.Globalization;
+//using System.Text.RegularExpression;
+
+class Run
+{
+    //public bool spellCheck = false;
+    public static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("us-US");
+        float[] r = new float[0];
+        //Console.WriteLine(r[r.Length-1] + "" + r.Length);
+        //Array.Resize(ref r, r.Length+1);
+        //Console.WriteLine(r[r.Length-1] + "" + r.Length);
+        bool rd = false;
+        string qs;
+
+        start();
+        void start()
+        {
+            switch (rd)
+            {
+                case false:
+                    Console.WriteLine("\nThis project is for calculating the circumference of multiple circles,\n" +
+                    "and then outputs the sum of the circumferences\n" +
+                    "to continue, please enter the radius of your circle...");
+                    rd = true;
+                    ask();
+                    break;
+                case true:
+                    Console.WriteLine("\nto continue, please enter the radius of your circle...");
+                    ask();
+                    break;
+
+            }
+        }
+        void ask()
+        {
+            qs = Console.ReadLine();
+            bool checke = check(qs);
+            if (checke)
+            {
+                //new Dic
+                Array.Resize(ref r, r.Length + 1);
+                r[r.Length - 1] = float.Parse(qs);
+                Console.Write("\nYour circle got the circumference of: " + 2 * r[r.Length - 1] * Math.PI);
+                if (r.Length > 1)
+                {
+                    float sum = 0;
+                    Console.Write("\nAnd gives you a circumference sum of: ");
+                    for (int i = 1; i <= r.Length; i++)
+                    {
+                        sum += 2 * r[i - 1] * MathF.PI;
+                    }
+                    Console.Write(sum);
+
+                }
+                more();
+            }
+            else
+            {
+                error1();
+            }
+        }
+        bool check(string str)
+        {
+            float f = 0;
+            bool b = float.TryParse(str, out f);
+            return b;
+        }
+        void more()
+        {
+            Console.WriteLine("\nWould you like to add another radius? Y/n");
+            string read = Console.ReadLine();
+            if (read.ToUpper() == "Y")
+            {
+                start();
+            }
+
+            //Environment.Exit(0);
+        }
+        void error1()
+        {
+            Console.WriteLine("your text could not be converted to type float\n" +
+                "Press any [Key] to reset");
+            Console.ReadKey();
+            start();
+        }
+        void error2()
+        {
+
+        }
+    }
+}
